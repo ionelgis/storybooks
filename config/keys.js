1e5c8a7c94f36e3d50454a9c8e809895ff590faf
ionelgis <ionelgis@gmail.com>
2017-11-01 16:12:14 +0200

    created google auth client id and secret

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://ionel:ionel@ds123930.mlab.com:23930/storybooks-dev',
+  googleClientID: '5872093707-7bdt7td6cc5642j6vhgu6s7ng89jtaf1.apps.googleusercontent.com',
+  googleClientSecret: '8Ob46EN5nAs5qlsF0uhQYL0_'
+}
