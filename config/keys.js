a9983e329ea197c8e8ec950ba1930bc99aad6209
ionelgis <ionelgis@gmail.com>
2017-11-08 16:23:47 +0200

    version 1.0.0 done

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://ionel:ionel@ds123930.mlab.com:23930/storybooks-dev',
-  googleClientID: '5872093707-7bdt7td6cc5642j6vhgu6s7ng89jtaf1.apps.googleusercontent.com',
-  googleClientSecret: '8Ob46EN5nAs5qlsF0uhQYL0_'
+if(process.env.NODE_ENV == 'production'){
+  module.exports = require('./keys_prod')
+}else{
+  module.exports = require('./keys_dev')
 }
